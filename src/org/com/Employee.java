3e8840df481d8150fe635903bb236d70b6a7c159
JebinArockiaa <arockiajebin@gmail.com>
2025-01-08 14:30:47 +0530

    First Time

---
+++ src/org/com/Employee.java
@@ -0,0 +1,15 @@
+package org.com;
+
+public class Employee {
+	
+	public void empName() {
+		System.out.println("Arockia");
+	}
+
+	public static void main(String[] args) {
+		Employee e= new Employee();
+		e.empName();
+
+	}
+
+}
