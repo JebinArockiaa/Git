09ad0886a1c5c650f541f4449ae284f45c9ca6c4
JebinArockiaa <arockiajebin@gmail.com>
2025-01-08 19:36:05 +0530

    Added new method

--- src/org/com/Employee.java
+++ src/org/com/Employee.java
@@ -5,10 +5,15 @@ public class Employee {
 	public void empName() {
 		System.out.println("Arockia");
 	}
+	
+	public void empId() {
+		System.out.println("SCIO-1900");
+	}
 
 	public static void main(String[] args) {
 		Employee e= new Employee();
 		e.empName();
+		e.empId();
 
 	}
 
